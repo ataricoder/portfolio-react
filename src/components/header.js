1b70029cfbb4946fbb4cfd6be83ccf3f00db80ba
ataricoder <ataricoder@gmail.com>
2019-01-23 00:33:38 -0600

    some header style changes, and also some media adaptive style changes in header and index

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './header.css'
 
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
-      <Link to="/"><img src="" width="30" /></Link>
+      <Link to="/"><img src="https://cl.ly/a77633d114e1/logo-designcode.svg" width="30" /></Link>
       <Link to="/courses">Courses</Link>
       <Link to="/downloads">Downloads</Link>
       <Link to="/workshops">Workshops</Link>
