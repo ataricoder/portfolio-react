ec56a608654bc2e2235f7551e1d081e58a012f07
ataricoder <ataricoder@gmail.com>
2019-01-22 23:56:48 -0600

    added some links to the header

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,14 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Header = ({ siteTitle }) => (
-  <div>
-    Header
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/"><img src="" width="30" /></Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">Downloads</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/buy">Buy</Link>
+    </div>
   </div>
 )
 
